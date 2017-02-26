790a3fa334dbe20fef4ea1100b6b2a9b75321dc8
Rob Solomon <bobsol@gmail.com>
2017-02-26 17:32:14 -0500

    several changes to dom animate page

--- js/domanimate.js
+++ js/domanimate.js
@@ -1,15 +1,41 @@
 var funcs = [],
     timer;
 
-funcs.push (function() { document.getElementsByTagName("MAIN")[0].style.backgroundColor="#587058"; });
-funcs.push (function() { document.getElementsByTagName("SECTION")[0].style.backgroundColor="#705858" });
-funcs.push (function() { document.getElementById("my").style.visibility="visible" });
-funcs.push (function() { document.getElementsByTagName("P")[1].style.visibility="visible" });
-funcs.push (function() { document.getElementById("a").src="../media/b.png" });
+funcs.push (function() { 
+    document.getElementById("my").style.visibility="visible";
+    document.getElementsByTagName("li")[0].style.visibility="visible";
+});
+
+funcs.push (function() { 
+    document.getElementsByTagName("MAIN")[0].style.backgroundColor="#587058"; 
+    document.getElementsByTagName("li")[1].style.visibility="visible";
+});
+
+funcs.push (function() { 
+    document.getElementsByTagName("SECTION")[0].style.backgroundColor="#705858";
+    document.getElementById("my").style.color="#dcdcdc";
+});
+
+funcs.push (function() { 
+    fade(document.getElementById("a"), document.getElementById("b"));
+});
 
 timer = setInterval(function() {
     funcs.shift()();
     if (!funcs.length) {
         clearInterval (timer);
     }
-}, 1500);
+}, 2000);
+
+function fade (firstObject, secondObject, fadeTime=5000) {
+    var count = 1,
+    timer = setInterval(function() {
+      /* step the fade */
+      count -= .01;
+      firstObject.style.opacity=count;
+      secondObject.style.opacity=1 - count;
+      if (count <= 0) {
+	  clearInterval (timer);
+      }
+    }, fadeTime/100);
+}
